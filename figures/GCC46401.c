4d84abe3662df536fb96f4fc9f0494b7eb7aa524
Shan Lu <shanlu@shanlu-compute1.cs.uchicago.edu>
2015-11-17 16:35:35 -0600

    finish section 2

--- figures/GCC46401.c
+++ figures/GCC46401.c
@@ -1,31 +1,9 @@
-// Bad input contains a long expression:
-// QStringList function_list=QStringList() 
-// <<"abasep" <<"abs" <<"absint" ... 
-
-// c-common.c
-static bool
-candidate_equal_p (const_tree x, const_tree y)
-{
-  return (x == y) || (x && y && 
-                   operand_equal_p (x, y, 0));
-}
-
-// Comments from developers: 
-// "No point tracking CALL_EXPRs that aren't 
-// ECF_CONST (because then operand_equal_p 
-// fails anyway) nor STRING_CSTs (which can't be 
-// written into)"
-static void
-merge_tlist(...)
-{
-  ...
-  for (tmp2 = *to; tmp2; tmp2 = tmp2->next)
-    if (candidate_equal_p (tmp2->expr, add->expr))
++ if(warning_candidate_p(to)) {
+   for (tmp = *to; tmp; tmp = tmp->next)
+    if (candidate_equal_p (tmp->expr, add->expr)
+         && !tmp->writer)
     { 
-      found = 1;
-      if (!tmp2->writer)
-        tmp2->writer = add->writer;
+      ...
+      tmp->writer = add->writer;
     }
-  ...
-}
-
++ }
--- figures/Mozilla477564.js
+++ figures/Mozilla477564.js
@@ -1,13 +1,9 @@
-1  //nsSessionStore.js
-2  generate: function sss_xph_generate(aNode)
-3  {
-4    ...
-5    for (let n = aNode; (n = n.previousSibling); )
-6      if (n.localName == aNode.localName 
-7          && n.namespaceURI == aNode.namespaceURI 
-8          && (!nName || n.name == nName))
-9        count++;
-10   //count will be used to generate a ID 
-11   //for the aNode
-12   ...
-13 }
+void sss_xph_generate(node_t* aNode)
+{
+   ...
+   for (n = aNode; n ; n = aNode->prev)
+     if (n->localName == aNode->localName 
+         && n->namespaceURI == aNode->namespaceURI) 
+         count++;
+     ...
+}//this function is called for every node aNode
