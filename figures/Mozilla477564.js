add4a81ba26b698b71ce5857e2a81afa1957e365
Shan Lu <shanlu@shanlu-compute1.cs.uchicago.edu>
2015-11-18 13:07:10 -0600

    near the end of section 3 revision

--- figures/Mozilla477564.js
+++ figures/Mozilla477564.js
@@ -1,9 +1,9 @@
 char * sss_xph_generate(node_t* aNode)
 {
-   ...
+   int count=0;
    for (n = aNode; n ; n = aNode->prev)
      if (n->localName == aNode->localName 
          && n->namespaceURI == aNode->namespaceURI) 
          count++;
-     ...
-}//this function is called for every node aNode
+   ...
+}//called for every node in a list
